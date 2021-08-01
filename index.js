58e2dce4066f40845b69f96002151baee135c168
Gagan Singh <randhawa_gagan@live.com>
2021-08-01 09:14:37 +0000

    Prepend

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList{
 
   constructor(value){
@@ -10,10 +17,7 @@ class LinkedList{
   }
 
   append(value){
-    const newNode = {
-      value: value,
-      next: null
-    }
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -21,12 +25,22 @@ class LinkedList{
   }
 
   prepend(value){
-    const newNode = 
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
 
 }
 
+
 const myLinkedList = new LinkedList(10);
+
 myLinkedList.append(5);
 myLinkedList.append(16);
+
+myLinkedList.prepend(1);
+
 
